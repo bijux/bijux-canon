6b971b497b54b85cb1e2b11ba9805c4eefed728f
bijan <mousavi.bijan@gmail.com>
2026-04-04 20:03:53 +0200

    refactor(make-help): merge shared help sections across modules

--- makes/help.mk
+++ makes/help.mk
@@ -8,9 +8,40 @@ HELP_WIDTH ?= 20
 
 ifeq ($(HELP_DEFINE_TARGET),1)
 $(HELP_TARGET):
-	@awk 'BEGIN{FS=":.*##"; OFS="";} \
-	  /^##@/ {gsub(/^##@ */,""); print "\n\033[1m" $$0 "\033[0m"; next} \
-	  /^[a-zA-Z0-9_.-]+:.*##/ {printf "  \033[36m%-$(HELP_WIDTH)s\033[0m %s\n", $$1, $$2}' \
+	@awk 'BEGIN{FS=":.*##"; OFS=""; section=""} \
+	  function add_section(name) { \
+	    if (!(name in section_seen)) { \
+	      section_seen[name] = 1; \
+	      section_order[++section_count] = name; \
+	    } \
+	  } \
+	  /^##@/ { \
+	    gsub(/^##@ */,""); \
+	    section = $$0; \
+	    add_section(section); \
+	    next; \
+	  } \
+	  /^[a-zA-Z0-9_.-]+:.*##/ { \
+	    if (section == "") { \
+	      section = "General"; \
+	      add_section(section); \
+	    } \
+	    section_lines[section] = section_lines[section] sprintf("  \033[36m%-$(HELP_WIDTH)s\033[0m %s\n", $$1, $$2); \
+	  } \
+	  END { \
+	    for (i = 1; i <= section_count; i++) { \
+	      name = section_order[i]; \
+	      if (section_lines[name] == "") { \
+	        continue; \
+	      } \
+	      if (printed_any) { \
+	        print ""; \
+	      } \
+	      print "\033[1m" name "\033[0m"; \
+	      printf "%s", section_lines[name]; \
+	      printed_any = 1; \
+	    } \
+	  }' \
 	  $(HELP_FILES)
 .PHONY: $(HELP_TARGET)
 endif
