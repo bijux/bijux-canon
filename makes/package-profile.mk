5d50c39199096b35146baef830c95b832f429689
bijan <mousavi.bijan@gmail.com>
2026-04-04 20:19:36 +0200

    refactor(make-layout): remove superseded package helper entrypoints

--- makes/package-profile.mk
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-# Copyright © 2026 Bijan Mousavi
-
-PACKAGE_PROFILE_MAKEFILE ?= $(abspath $(lastword $(MAKEFILE_LIST)))
-PACKAGE_MAKEFILE_DIR ?= $(abspath $(dir $(PACKAGE_PROFILE_MAKEFILE)))
-PROJECT_SLUG ?= $(basename $(notdir $(PACKAGE_PROFILE_MAKEFILE)))
-
-include $(PACKAGE_MAKEFILE_DIR)/../env.mk
-include $(PACKAGE_MAKEFILE_DIR)/../path-sets.mk
