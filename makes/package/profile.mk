b46b3e98834a7b1b42630c7effb539d7131cb023
bijan <mousavi.bijan@gmail.com>
2026-04-08 22:36:30 +0200

    refactor: derive canon package identity from package directory

--- makes/package/profile.mk
+++ makes/package/profile.mk
@@ -3,7 +3,8 @@
 
 PACKAGE_PROFILE_MAKEFILE ?= $(abspath $(firstword $(MAKEFILE_LIST)))
 PACKAGE_MAKEFILE_DIR ?= $(abspath $(dir $(PACKAGE_PROFILE_MAKEFILE)))
-PROJECT_SLUG ?= $(basename $(notdir $(PACKAGE_PROFILE_MAKEFILE)))
+PROJECT_DIR ?= $(CURDIR)
+PROJECT_SLUG ?= $(notdir $(abspath $(PROJECT_DIR)))
 
 include $(PACKAGE_MAKEFILE_DIR)/../env.mk
 include $(ROOT_MAKE_DIR)/package/path-sets.mk
