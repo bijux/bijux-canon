31eff50e66df55fdcd0bfd45beb9bb05bb19c99d
bijan <mousavi.bijan@gmail.com>
2026-04-09 03:36:13 +0200

    refactor(make): flatten shared api modules

--- makes/api-freeze.mk
+++ makes/api-freeze.mk
@@ -1,4 +1,4 @@
 API_FREEZE_COMMAND ?= $(VENV_PYTHON) -m bijux_canon_dev.api.freeze_contracts --repo-root "$(MONOREPO_ROOT)"
 API_OPENAPI_DRIFT_COMMAND ?=
 
-include $(ROOT_MAKE_DIR)/bijux-py/api/freeze.mk
+include $(ROOT_MAKE_DIR)/bijux-py/api-freeze.mk
